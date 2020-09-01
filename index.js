eaeee9c4396c41a758d50f2b1f6e8f487521dbdc
Zach Bryan <zacharybryan@icloud.com>
2020-09-01 02:39:48 -0700

    added prompts

--- index.js
+++ index.js
@@ -6,5 +6,49 @@ inquirer
         {
             name: 'name',
             message: 'What is your name?'
+        },
+        {
+            name: 'email',
+            message: 'What is your email?'
+        },
+        {
+            name: 'githubUserName',
+            message: 'What is your Github user name?'
+        },
+        {
+            name: 'projectTitle',
+            message: 'What is the name of your project?'
+        },
+        {
+            name: 'description',
+            message: 'Describe your project'
+        },
+        {
+            name: 'videoLink',
+            message: 'What is the url of your projects demo video?'
+        },
+        {
+            name: 'installation',
+            message: 'What are the steps to install?'
+        },
+        {
+            name: 'usage',
+            message: 'What are usage instructions?'
+        },
+        {
+            name: 'testingSteps',
+            message: 'What are the steps to test your app?'
+        },
+        {
+            name: 'licence',
+            message: 'What licence would you like to use?'
+        },
+        {
+            name: 'contributorsName',
+            message: 'What are names of your contributors?'
+        }, 
+        {
+            name: 'contributorsGithub',
+            message: 'What are their Github Usernames?'
         }
 ]);
