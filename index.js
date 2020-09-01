4d9ab87b989534c8c7b758e02c344dca9ad5379b
Zach Bryan <zacharybryan@icloud.com>
2020-09-01 03:41:21 -0700

    able to console log incorporated readme template

--- index.js
+++ index.js
@@ -66,10 +66,51 @@ inquirer
     const contributorsName = answers.contributorsName;
     const contributorsGithub = answers.contributorsGithub;
 
+    const readMeOutput = `# ${projectTitle}
+
+    ## Description
     
+    ${description}
+    
+    ### Table of Contents
+    
+    * [Description] (#description)
+    * [Installation] (#installation)
+    * [Usage] (#usage)
+    * [Contributing] (#contributing)
+    * [Tests] (#tests)
+    * [Questions] (#questions)
+    * [License] (#licence)
+    
+    ### Installation
+    
+    [Watch the demo here!](${videoLink})
+    ${installation}
+    
+    ### Usage
+    
+    [Watch the demo here!](${videoLink})
+    ${usage}
+    
+    ### Contributing
+    
+    [${contributorsName}] (https://www.github.com/${contributorsGithub})
+    
+    ### Tests
+    
+    ${testingSteps}
+    
+    ### Questions
+    
+    [${githubUserName}] (https://www.github.com/${githubUserName})
+    
+    For any additional questions  or update requests please contact me at ${email}
+    
+    ### License
+    
+    Copyright (c) 2020 ${name}
+    
+    ${licence}`
 
-
-
-
-
+    console.log(readMeOutput);
 });
